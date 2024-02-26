cadedd4c1a7370c31ab8cdb077180795655cceda
samuelsnv2 <samuelsnv2@gmail.com>
2024-02-26 20:36:35 -0300

    site funcionando porem datas anteriores

--- reminder-app/src/LembreteSistema.js
+++ reminder-app/src/LembreteSistema.js
@@ -5,27 +5,45 @@ class LembreteSistema extends Component{
     constructor(props){
         super(props);
         this.state = {
-            lembretes: {}
-        }
+            lembretes: {},
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     criarLembrete = (nome, data) => {
-        const novoLembrete = new Lembrete(nome, data);
-        const dataFormatada = novoLembrete.getData().toLocaleDateString();
+        const dataCerta = new Date(data);
+        const dataFormatada = dataCerta.toLocaleDateString();
 
-        this.setState(prevState =>{
-            // const lembretes = {nome: novoLembrete.getNome(), data: dataFormatada};
-            const lembretes = { ...prevState.lembretes}
+        const existeLembretes = this.state.lembretes[dataFormatada] || [];
+        const estaDuplicado = existeLembretes.some(reminder => reminder.nome === nome);
+
+        if (!estaDuplicado) {
+            const novoLembrete = new Lembrete(nome, dataCerta);
+            this.setState(prevState => {
+                const lembretes = { ...prevState.lembretes };
+                if (lembretes[dataFormatada]) {
+                    lembretes[dataFormatada].push(novoLembrete);
+                } else {
+                    lembretes[dataFormatada] = [novoLembrete];
+                }
+                return { lembretes };
+            });
+        } else {
+            console.log('Lembrete já existe');
+        }
+    };
+
+    deletaLembrete = (nome, data) => {
+        const dataFormatada = data.toLocaleDateString();
+        this.setState(prevState => {
+            const lembretes = { ...prevState.lembretes };
             if(lembretes[dataFormatada]){
-                lembretes[dataFormatada].push(novoLembrete);
+                lembretes[dataFormatada] = lembretes[dataFormatada].filter(
+                    lembrete => lembrete.nome !== nome
+                );
             }
-            else{
-                lembretes[dataFormatada] = [novoLembrete];
-            }
-            console.log(lembretes[dataFormatada]);
-            return {lembretes};
+            return { lembretes };
         });
-        
     };
 
     ordenaDatas = () => {
@@ -36,30 +54,41 @@ class LembreteSistema extends Component{
         }));
     };
 
+    handleSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const nome = event.target.nome.value;
+        const data = new Date(form.elements.data.value);
+        this.criarLembrete(nome, data);
+        form.reset();
+    }
+
     render(){
+        const lembretesOrdenados = this.ordenaDatas();
         return(
             <div>
-                <h1>Lembrete</h1>
-                <form id="reminder-form" onSubmit={this.criarLembrete}>
-                    <input type="text" id="name" placeholder="Nome" />
-                    <input type="date" id="date" />
+                <h2>Sistema de Lembretes</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Nome:
+                        <input type="text" name="nome" />
+                    </label>
+                    <label>
+                        Data:
+                        <input type="date" name="data" />
+                    </label>
                     <button type="submit">Adicionar Lembrete</button>
                 </form>
-                <ul id="reminder-list">
-                    {this.state.lembretes.map((lembrete, index) => (
-                        <li key={index}>
-                            {lembrete.data}:
-                            {lembrete.lembretes.map((lembreteItem, lembreteIndex) => (
-                                <div key={lembreteIndex}>
-                                    <p>{lembreteItem.nome}</p>
-                                </div>
+                {lembretesOrdenados.map((lembreteData, index) => (
+                    <div key={index}>
+                        <h3>{lembreteData.data}</h3>
+                        <ul>
+                            {lembreteData.lembretes.map((lembrete, idx) => (
+                                <li key={idx}>{lembrete.nome}</li>
                             ))}
-                        </li>
-                    ))}
-                    
-                    {/* /* {this.state.lembretes.map(lembrete => <li key={lembrete.nome}>{lembrete}</li>)} */}
-
-                </ul>
+                        </ul>
+                    </div>
+                ))}
             </div>
         );
     };
